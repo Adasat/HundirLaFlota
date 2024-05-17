e328318a691c05fd77ba13a128b8b56dc56124e6
Adasat <pbonbol@gmail.com>
2024-05-17 10:10:16 +0100

    Create function to fill tablero

---
+++ index.js
@@ -0,0 +1,40 @@
+const container = document.querySelector('#container')
+
+console.log(container)
+
+/*  EL TABLERO: 8 x 8
+    x x x x x x x x
+    x x x x x x x x
+    x x x x x x x x
+    x x x x x x x x
+    x x x x x x x x
+    x x x x x x x x
+    x x x x x x x x
+*/
+
+function fillTablero () {
+    const tablero = document.createElement('div')
+    tablero.setAttribute('id', 'tablero')
+    let cell = ''
+
+    for (let i = 0; i < 64; i++){
+        cell += `<div id=${i}>CELL ${i}</div>`
+    }
+
+    tablero.innerHTML = cell
+
+    console.log(tablero)
+
+    container.appendChild(tablero)
+
+    
+
+}
+
+
+
+
+window.onload = function () {
+    fillTablero()
+   
+}
